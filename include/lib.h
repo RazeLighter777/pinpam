5998bbdf8edff7c7f0c039f711f2e86c4aa12119
RazeLighter777 <justin.suess@westpoint.edu>
2025-10-16 11:22:28 -0400

    builds

---
+++ include/lib.h
@@ -0,0 +1,5 @@
+#include <security/pam_modules.h>
+
+int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc,
+                               const char **argv);
+
---
+++ src/lib.c
@@ -0,0 +1,4 @@
+#include "lib.h"
+int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    return PAM_SUCCESS;
+}
