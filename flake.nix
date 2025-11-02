73159955d3ba6eaf947156da6d2b53dd64cd2e50
RazeLighter777 <gorgonballs@proton.me>
2025-11-02 14:57:30 -0500

    Add unpackPhase to build phases in flake.nix

--- flake.nix
+++ flake.nix
@@ -80,7 +80,7 @@
 
               runHook postInstall
             '';
-			phases = [ "buildPhase" "installPhase"];
+			phases = [ "unpackPhase" "buildPhase" "installPhase"];
 
             meta = with pkgs.lib; {
               description = "TPM-backed PIN authentication PAM module";
